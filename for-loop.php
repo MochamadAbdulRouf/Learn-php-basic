87b23b61d6695d966f4dba0401a2e6c01ad901f6
Rouf -Myz <rouf08412@gmail.com>
2025-07-01 15:06:45 +0700

    commit done learn for-loop

--- for-loop.php
+++ for-loop.php
@@ -1,2 +1,30 @@
 <?php
+$counter1 = 1;
+for ( ; $counter1 <= 10 ;){
+    echo "ini adalah for loop ke-$counter1" . PHP_EOL;
+    $counter1++;
+}
 
+#perulangan dengan init statement
+for ($counter = 1; $counter <= 10;) {
+    echo "hello for loop : " . $counter . PHP_EOL;
+    $counter++;
+}
+
+#perulangan dengan post statement
+for ($counter = 1; $counter <= 10; $counter++) {
+    echo "hello for loop : " . $counter . PHP_EOL;
+}
+
+for ($counter = 10; $counter >= 1; $counter--) {
+    echo "hello for loop : " . $counter . PHP_EOL;
+}
+
+#Syntax alternative for loop 
+for ($counter = 1; $counter <= 10; $counter++) :
+    echo "Hei for loop : " . $counter . PHP_EOL;
+endfor;
+
+for ($counter = 10; $counter >= 1; $counter--) :
+    echo "hello for loop : " . $counter . PHP_EOL;
+endfor;
